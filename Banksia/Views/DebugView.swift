8434890bebcee2083e20e7681aa6398b1bc89a34
Dave Coleman <dave@dvclmn.com>
2024-05-29 17:19:04 +0200

    Improves Debug window styling

--- Banksia/Views/DebugView.swift
+++ Banksia/Views/DebugView.swift
@@ -9,18 +9,51 @@ import SwiftUI
 import GeneralStyles
 import Grainient
 import Sidebar
+import Swatches
 
 struct DebugInfo: Identifiable {
     var id = UUID()
     var title: String
     var state: String
-    var definedOn: String
+    var definedOn: DefinedOn
 }
 
-enum SortingOption {
+
+enum DebugColumn {
     case title
     case state
     case definedOn
+    
+    var title: String {
+        switch self {
+        case .title:
+            "Title"
+        case .state:
+            "State"
+        case .definedOn:
+            "Defined on"
+        }
+    }
+}
+
+enum DefinedOn {
+    case conv
+    case sidebar
+    
+    var name: String {
+        switch self {
+        case .conv:
+            "ConversationHandler"
+        case .sidebar:
+            "SidebarHandler"
+        }
+    }
+}
+
+enum ColumnPosition {
+    case beginning
+    case middle
+    case end
 }
 
 struct DebugView: View {
@@ -42,7 +75,7 @@ struct DebugView: View {
     
     
     
-    @State private var sorting: SortingOption = .title
+    @State private var sorting: DebugColumn = .title
     
     @State private var isHoveringDebug: Bool = false
     
@@ -53,23 +86,37 @@ struct DebugView: View {
             DebugInfo(
                 title: "Editor focused",
                 state: "\(conv.isEditorFocused)",
-                definedOn: "\(ConversationHandler.self)"
+                definedOn: .conv
             ),
             DebugInfo(
                 title: "Sidebar visible",
                 state: "\(sidebar.isSidebarVisible)",
-                definedOn: "\(sidebar)"
+                definedOn: .sidebar
             ),
             DebugInfo(
-                title: "Sidebar user-dismissed",
+                title: "Sidebar dismissed",
                 state: "\(sidebar.isSidebarDismissed)",
-                definedOn: "\(SidebarHandler.self)"
+                definedOn: .sidebar
             )
         ]
         
+//        let columns: [GridItem] = [
+//            GridItem(.adaptive(minimum: 40), alignment: .topLeading)
+//        ]
+        
+        let spacing: Double = 0
+        
         let columns: [GridItem] = [
-            GridItem(.adaptive(minimum: 100, maximum: 300), alignment: .topLeading)
+            GridItem(.flexible(minimum: 40),                spacing: spacing, alignment: .topLeading),
+            GridItem(.flexible(minimum: 40, maximum: 80),   spacing: spacing, alignment: .topLeading),
+            GridItem(.flexible(minimum: 120),                spacing: spacing, alignment: .topLeading)
         ]
+        
+//        let columns: [GridItem] = [
+//            GridItem(.fixed(120), alignment: .topLeading),
+//            GridItem(.fixed(120), alignment: .topLeading),
+//            GridItem(.fixed(120), alignment: .topLeading)
+//        ]
         
         var sortedDebugInfo: [DebugInfo] {
             switch sorting {
@@ -78,7 +125,7 @@ struct DebugView: View {
             case .state:
                 return debugInfo.sorted { $0.state < $1.state }
             case .definedOn:
-                return debugInfo.sorted { $0.definedOn < $1.definedOn }
+                return debugInfo.sorted { $0.definedOn.name < $1.definedOn.name }
             }
         }
         
@@ -96,9 +143,20 @@ struct DebugView: View {
                 
                 
                 
-                TableColumn(header: "Title", rows: sortedDebugInfo.map { $0.title })
-                TableColumn(header: "State", rows: sortedDebugInfo.map { $0.state })
-                TableColumn(header: "Defined on", rows: sortedDebugInfo.map { $0.definedOn })
+                CustomTableColumn(
+                    column: DebugColumn.title,
+                    rows: sortedDebugInfo.map { $0.title },
+                    columnPosition: .beginning
+                )
+                CustomTableColumn(
+                    column: DebugColumn.state,
+                    rows: sortedDebugInfo.map { $0.state }
+                )
+                CustomTableColumn(
+                    column: DebugColumn.definedOn,
+                    rows: sortedDebugInfo.map { $0.definedOn.name },
+                    columnPosition: .end
+                )
                 
                 
                 //                VStack(alignment: .leading) {
@@ -139,31 +197,95 @@ struct DebugView: View {
 
 extension DebugView {
     @ViewBuilder
-    func TableHeader(_ label: String) -> some View {
+    func CustomTableHeader(_ label: String) -> some View {
         Text(label)
             .fontWeight(.bold)
     }
     
     @ViewBuilder
-    func TableRow(_ content: String) -> some View {
-        Text(content)
+    func CustomTableRow(_ content: String, column: DebugColumn) -> some View {
+        
+        var booleanStyle: Color {
+            if content == "true" {
+                Swatch.eggplant.colour
+            } else if content == "false" {
+                Swatch.peach.colour.opacity(0.8)
+            } else {
+                .secondary
+            }
+        }
+        
+        Group {
+            switch column {
+            case .title:
+                Text(content)
+            case .state:
+                Text(content)
+                    .monospaced()
+                    .foregroundStyle(booleanStyle)
+                    .padding(.horizontal, 3)
+                    .padding(.vertical, 1)
+                    .background(.black.opacity(0.3))
+                    .clipShape(.rect(cornerRadius: Styles.roundingTiny))
+            case .definedOn:
+                Text(content)
+                    .foregroundStyle(.secondary)
+            }
+        }
             .frame(height: 20)
     }
     
-    //    @ViewBuilder
-    //    func TableColumn(_ content: String) -> some View {
-    //        Text(content)
-    //            .frame(height: 20)
-    //    }
-    
     @ViewBuilder
-    func TableColumn(header: String, rows: [String]) -> some View {
-        VStack(alignment: .leading) {
-            TableHeader(header)
-            ForEach(rows, id: \.self) { row in
-                TableRow(row)
+    func CustomTableColumn(
+        column: DebugColumn,
+        rows: [String],
+        columnPosition: ColumnPosition = .middle
+    ) -> some View {
+        
+        let rounding: Double = Styles.roundingSmall
+        
+        var corners: UnevenRoundedRectangle {
+            switch columnPosition {
+            case .beginning:
+                return .rect(
+                    topLeadingRadius: rounding,
+                    bottomLeadingRadius: rounding,
+                    bottomTrailingRadius: 0,
+                    topTrailingRadius: 0
+                )
+            case .middle:
+                return .rect(
+                    topLeadingRadius: 0,
+                    bottomLeadingRadius: 0,
+                    bottomTrailingRadius: 0,
+                    topTrailingRadius: 0
+                )
+            case .end:
+                return .rect(
+                    topLeadingRadius: 0,
+                    bottomLeadingRadius: 0,
+                    bottomTrailingRadius: rounding,
+                    topTrailingRadius: rounding
+                )
             }
         }
+        
+        
+        VStack(alignment: .leading, spacing: 0) {
+            CustomTableHeader(column.title)
+                .padding(.horizontal, 6)
+                .padding(.bottom, 6)
+            ForEach(Array(zip(rows.indices, rows)), id: \.0) { index, row in
+                CustomTableRow(row, column: column)
+                    .padding(.horizontal, 6)
+                    .padding(.vertical, 2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(index % 2 == 0 ? Color.black.opacity(0.1) : Color.clear)
+                    .clipShape(corners)
+                    
+            }
+        }
+        
     }
 }
 
