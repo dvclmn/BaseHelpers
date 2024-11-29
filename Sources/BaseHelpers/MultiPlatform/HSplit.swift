a4a7552bfa1a2fd3c7f6de824e38a7d2be764770
Dave Coleman <dave@dvclmn.com>
2024-11-29 12:36:38 +0100

    Moves HSplit to Components package

--- Sources/BaseHelpers/MultiPlatform/HSplit.swift
+++ Sources/BaseHelpers/MultiPlatform/HSplit.swift
@@ -6,33 +6,55 @@
 //
 
 import SwiftUI
+//
+//public struct HSplit<Content: View>: View {
+//  
+//  let content: Content
+//  
+//  public init(@ViewBuilder content: @escaping () -> Content) {
+//    self.content = content()
+//  }
+//  
+//  public var body: some View {
+//#if canImport(AppKit)
+//    HSplitView {
+//      content
+//    }
+//#elseif canImport(UIKit)
+//    HStack {
+//      content
+//    }
+//#endif
+//  }
+//}
 
-#if os(macOS)
-struct HSplit<Content: View>: View {
-  let content: Content
-  
-  init(@ViewBuilder content: () -> Content) {
-    self.content = content()
-  }
-  
-  var body: some View {
-    HSplitView {
-      content
-    }
-  }
-}
-#else
-struct HSplit<Content: View>: View {
-  let content: Content
-  
-  init(@ViewBuilder content: () -> Content) {
-    self.content = content()
-  }
-  
-  var body: some View {
-    HStack {
-      content
-    }
-  }
-}
-#endif
+
+//#if os(macOS)
+//struct HSplit<Content: View>: View {
+//  let content: Content
+//  
+//  init(@ViewBuilder content: () -> Content) {
+//    self.content = content()
+//  }
+//  
+//  var body: some View {
+//    HSplitView {
+//      content
+//    }
+//  }
+//}
+//#else
+//struct HSplit<Content: View>: View {
+//  let content: Content
+//  
+//  init(@ViewBuilder content: () -> Content) {
+//    self.content = content()
+//  }
+//  
+//  var body: some View {
+//    HStack {
+//      content
+//    }
+//  }
+//}
+//#endif
