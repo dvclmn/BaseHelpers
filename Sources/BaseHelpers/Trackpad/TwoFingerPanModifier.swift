b2d7cf06274c4e216f19afb0640b936aee84fd26
Dave Coleman <dave@dvclmn.com>
2024-12-12 14:51:12 +0100

    Adds isPanning closure to PanModifier

--- Sources/BaseHelpers/Trackpad/TwoFingerPanModifier.swift
+++ Sources/BaseHelpers/Trackpad/TwoFingerPanModifier.swift
@@ -11,15 +11,18 @@ public struct TwoFingerPanModifier: ViewModifier {
   
   @Binding var panAmount: CGPoint
   @State private var panDelta: CGPoint = .zero
+  @State private var isCurrentlyPanning: Bool = false
+
   @State private var monitor: Any?
+  
   
   let sensitivity: CGFloat
   let isPanning: (Bool) -> Void
 
   public init(
     panAmount: Binding<CGPoint>,
-    sensitivity: CGFloat = 0.8,
-  isPanning: @escaping (Bool) -> Void = { _ in }
+    sensitivity: CGFloat,
+    isPanning: @escaping (Bool) -> Void
   ) {
     self._panAmount = panAmount
     self.sensitivity = sensitivity
@@ -32,6 +35,22 @@ public struct TwoFingerPanModifier: ViewModifier {
         monitor = NSEvent.addLocalMonitorForEvents(matching: .scrollWheel) { event in
           guard !event.scrollingDeltaX.isNaN && !event.scrollingDeltaY.isNaN else {
             return event
+          }
+          
+          // Check phase changes
+          switch event.phase {
+            case .began:
+              if !isCurrentlyPanning {
+                isCurrentlyPanning = true
+                isPanning(true)
+              }
+            case .ended, .cancelled:
+              if isCurrentlyPanning {
+                isCurrentlyPanning = false
+                isPanning(false)
+              }
+            default:
+              break
           }
           
           /// Update delta
@@ -53,6 +72,11 @@ public struct TwoFingerPanModifier: ViewModifier {
         if let monitor = monitor {
           NSEvent.removeMonitor(monitor)
         }
+        /// Reset the panning state when the view disappears
+        if isCurrentlyPanning {
+          isCurrentlyPanning = false
+          isPanning(false)
+        }
       }
   }
 }
@@ -61,12 +85,14 @@ public struct TwoFingerPanModifier: ViewModifier {
 extension View {
   public func panTrackpadGesture(
     _ panAmount: Binding<CGPoint>,
-    sensitivity: CGFloat = 0.8
+    sensitivity: CGFloat = 0.8,
+    isPanning: @escaping (Bool) -> Void = { _ in }
   ) -> some View {
     self.modifier(
       TwoFingerPanModifier(
         panAmount: panAmount,
-        sensitivity: sensitivity
+        sensitivity: sensitivity,
+        isPanning: isPanning
       )
     )
   }
