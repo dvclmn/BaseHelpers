5a7c082cca3cc9fbfd5a4e86276b5a67878bcd20
Dave Coleman <dave@dvclmn.com>
2025-01-13 14:47:09 +0100

    Updates sheet frame

--- Sources/BaseStyles/SheetFrame.swift
+++ Sources/BaseStyles/SheetFrame.swift
@@ -16,9 +16,11 @@ import SwiftUI
     let maxWidth: CGFloat
     let minHeight: CGFloat
     let maxHeight: CGFloat
+//    let presentatingSizing: PresentationSizing
 
     public func body(content: Content) -> some View {
       content
+//        .presentationSizing(.fitted)
         .frame(
           minWidth: minWidth,
           idealWidth: (minWidth + maxWidth) / 2,
@@ -31,22 +33,62 @@ import SwiftUI
   }
   extension View {
     
+    /// Example use:
+    /// ```
+    /// // Specifying width
+    /// someView.sheetFrame(2/3, width: 400)  // Portrait orientation (2:3)
+    /// someView.sheetFrame(16/9, width: 500)  // Landscape orientation (16:9)
+    ///
+    /// // Specifying height
+    /// someView.sheetFrame(2/3, height: 600)  // Portrait orientation (2:3)
+    /// someView.sheetFrame(16/9, height: 300)  // Landscape orientation (16:9)
+    ///
+    /// ```
+    /// Sets up a frame with a specific aspect ratio and width
+    /// - Parameters:
+    ///   - aspectRatio: The desired aspect ratio (width/height). For example, 2/3 represents a portrait orientation.
+    ///   - width: The desired width. This will be used to calculate the appropriate height.
+    ///   - minWidth: The minimum allowed width. Defaults to 340.
+    ///   - maxWidth: The maximum allowed width. Defaults to 680.
     public func sheetFrame(
+      _ aspectRatio: CGFloat,
+      width: CGFloat,
       minWidth: CGFloat = 340,
-      maxWidth: CGFloat = 680,
-      minHeight: CGFloat = 400,
-      maxHeight: CGFloat = 960
+      maxWidth: CGFloat = 680
     ) -> some View {
-      self.modifier(
+      let height = width / aspectRatio
+      return self.modifier(
         SheetFrame(
           minWidth: minWidth,
           maxWidth: maxWidth,
+          minHeight: height * (minWidth / width),
+          maxHeight: height * (maxWidth / width)
+        )
+      )
+    }
+    
+    /// Sets up a frame with a specific aspect ratio and height
+    /// - Parameters:
+    ///   - aspectRatio: The desired aspect ratio (width/height). For example, 2/3 represents a portrait orientation.
+    ///   - height: The desired height. This will be used to calculate the appropriate width.
+    ///   - minHeight: The minimum allowed height. Defaults to 400.
+    ///   - maxHeight: The maximum allowed height. Defaults to 960.
+    public func sheetFrame(
+      _ aspectRatio: CGFloat,
+      height: CGFloat,
+      minHeight: CGFloat = 400,
+      maxHeight: CGFloat = 960
+    ) -> some View {
+      let width = height * aspectRatio
+      return self.modifier(
+        SheetFrame(
+          minWidth: width * (minHeight / height),
+          maxWidth: width * (maxHeight / height),
           minHeight: minHeight,
           maxHeight: maxHeight
         )
       )
     }
-    
     
     public func sheetFrame(
       minWidth: CGFloat = 340,
