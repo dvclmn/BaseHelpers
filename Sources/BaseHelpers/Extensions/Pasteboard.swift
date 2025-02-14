8bd029b225b4c81d47295d6ee6345593c8168b17
Dave Coleman <dave@dvclmn.com>
2025-02-14 21:36:50 +0100

    I do not understand why there are so many new ...
    ... files in this commit, which aren't at all new (to me). Oh well

---
+++ Sources/BaseHelpers/Extensions/Pasteboard.swift
@@ -0,0 +1,44 @@
+//
+//  Pasteboard.swift
+//  Collection
+//
+//  Created by Dave Coleman on 14/2/2025.
+//
+
+import Foundation
+
+// MARK: - Copy to clipboard
+public protocol PasteboardCopying {
+  func setString(_ string: String)
+}
+
+#if canImport(AppKit)
+import AppKit
+
+extension NSPasteboard: PasteboardCopying {
+  public func setString(_ string: String) {
+    clearContents()
+    setString(string, forType: .string)
+  }
+}
+
+#elseif canImport(UIKit)
+import UIKit
+
+extension UIPasteboard: PasteboardCopying {
+  public func setString(_ string: String) {
+    self.string = string
+  }
+}
+#endif
+
+/// Usage
+public func copyStringToClipboard(_ contents: String) {
+  let pasteboard: PasteboardCopying
+#if canImport(AppKit)
+  pasteboard = NSPasteboard.general
+#elseif canImport(UIKit)
+  pasteboard = UIPasteboard.general
+#endif
+  pasteboard.setString(contents)
+}
