796493b54b1823806752db1aeac7d953b353f8c3
Dave Coleman <dave@dvclmn.com>
2025-05-15 12:40:04 +0200

    Going to try removing modular scale, and ...
    ... defining elsewhere, as it alone adds unneccesary complexity

--- Sources/BaseHelpers/ValueCurve/ValueCurveType.swift
+++ Sources/BaseHelpers/ValueCurve/ValueCurveType.swift
@@ -7,30 +7,27 @@
 
 import Foundation
 
-//public struct ValueCurve {
-//  public let type: ValueCurveType
-////  public let customFunction: ((Double) -> Double)?
-//
-//  public init(
-//    _ type: ValueCurveType,
-////    custom: ((Double) -> Double)? = nil
-//  ) {
-//    self.type = type
-////    self.customFunction = custom
-//  }
-//
-//  public func apply(to value: CGFloat) -> CGFloat {
-//    let f = type.function(custom: nil)
-////    let f = type.function(custom: customFunction)
-//    return CGFloat(f(Double(value)))
-//  }
-//}
-
-struct ValueCurve {
-  let transform: (Double) -> Double
-  func apply(_ input: Double) -> Double {
+public struct ValueCurve {
+  public let transform: (Double) -> Double
+  public func apply(_ input: Double) -> Double {
     transform(input)
   }
+}
+
+public struct CurveParameters {
+  public var baseRatio: Double?
+  public var steps: Int?
+  
+  public init(
+    baseRatio: Double? = nil,
+    steps: Int? = nil
+  ) {
+    self.baseRatio = baseRatio
+    self.steps = steps
+  }
+  // You could also add optional fields for other use-cases, like:
+  // var exponent: Double?
+  // var amplitude: Double?
 }
 
 public enum ValueCurveType: String, CaseIterable, Identifiable {
@@ -59,54 +56,33 @@ public enum ValueCurveType: String, CaseIterable, Identifiable {
     }
   }
   
-  
-  
-  func asCurve(settings: ScaleSettings) -> ValueCurve {
+  public func asCurve(using params: CurveParameters = .init()) -> ValueCurve {
     switch self {
       case .linear:
         return ValueCurve { $0 }
+        
       case .exponential:
         return ValueCurve { pow($0, 2) }
+        
       case .logarithmic:
         return ValueCurve { $0 == 0 ? 0 : log($0 + 1) / log(2) }
+        
       case .quadratic:
         return ValueCurve { pow($0, 2) }
+        
       case .cubic:
         return ValueCurve { pow($0, 3) }
+        
       case .sine:
         return ValueCurve { sin($0 * .pi / 2) }
+        
       case .modularScale:
-        let base = settings.baseRatio
+        let base = params.baseRatio ?? 1.125
+        let steps = Double(params.steps ?? 10)
         return ValueCurve { position in
-          let steps = position * Double(settings.steps - 1)
-          return pow(base, steps)
+          let i = position * (steps - 1)
+          return pow(base, i)
         }
     }
   }
-  
-  
-
-//  public func function(custom: ((Double) -> Double)? = nil) -> (Double) -> Double {
-//    switch self {
-//      case .linear:
-//        return { $0 }
-//      case .quadratic:
-//        return { pow($0, 2) }
-//      case .cubic:
-//        return { pow($0, 3) }
-//      case .exponential:
-//        return { 1 - exp(-$0 * 3) }
-//      case .logarithmic:
-//        return { log2($0 + 1) }  // or adjust as needed
-//      case .sine:
-//        return { sin($0 * .pi / 2) }
-//      case .modularScale:
-//        return { pow(1.25, $0 * 10) }  // example modular scale
-////      case .custom:
-////        guard let custom = custom else {
-////          fatalError("Custom curve requires a provided function.")
-////        }
-////        return custom
-//    }
-//  }
 }
