0a2f943dc5b25c5f245f00daaf22fe79397d1a78
Dave Coleman <dave@dvclmn.com>
2025-05-19 11:28:31 +0200

    Partway through adapting scrolling helpers...
    ... to provide a richer report, and combined into one

--- Sources/BaseHelpers/Scrolling/IsScrollContentsClipped.swift
+++ Sources/BaseHelpers/Scrolling/IsScrollContentsClipped.swift
@@ -7,19 +7,49 @@
 
 import SwiftUI
 
+struct HorizontalScrollState: Equatable {
+  var isClipped: Bool = false
+  var isAtStart: Bool = false
+  var isAtEnd: Bool = false
+}
+
+
 public struct IsScrollContentClippedModifier: ViewModifier {
   
-  let isClipped: (Bool) -> Void
+//  @State private var scrollState
+  
+  let scrollState: (HorizontalScrollState) -> Void
+//  let isClipped: (Bool) -> Void
   public func body(content: Content) -> some View {
     if #available(macOS 15.0, *) {
       content
-        .onScrollGeometryChange(for: Bool.self, of: { geometry in
-          let containerWidth = geometry.containerSize.width
-          let contentsWidth = geometry.contentSize.width
-          return containerWidth < contentsWidth
-        }, action: { _, newValue in
-          isClipped(newValue)
-        })
+        .onScrollGeometryChange(for: HorizontalScrollState.self) { geometry in
+            
+            /// Is Clipped
+            let containerWidth = geometry.containerSize.width
+            let contentsWidth = geometry.contentSize.width
+            let isClipped = containerWidth < contentsWidth
+            
+            /// Is at start/end
+            let visibleMinX = geometry.visibleRect.minX
+            let visibleMaxX = geometry.visibleRect.maxX
+            let contentMaxX = geometry.contentSize.width
+            let tolerance: CGFloat = 1.0
+            
+            let isAtStart = visibleMinX <= tolerance
+            let isAtEnd = visibleMaxX >= (contentMaxX - tolerance)
+
+            let state = HorizontalScrollState(
+              isClipped: isClipped,
+              isAtStart: isAtStart,
+              isAtEnd: isAtEnd
+            )
+            
+            return state
+            
+          } action: { _, newValue in
+            scrollState(newValue)
+        }
       
     } else {
       
@@ -27,6 +57,43 @@ public struct IsScrollContentClippedModifier: ViewModifier {
     }
   }
 }
+
+
+public struct IsScrolledToEnd: ViewModifier {
+  
+  let isScrolledToEnd: (Bool) -> Void
+  public func body(content: Content) -> some View {
+    if #available(macOS 15.0, *) {
+      content
+        .onScrollGeometryChange(
+          for: Bool.self,
+          of: { geometry in
+            
+           
+            
+            //            print(
+            //              """
+            //              Vis rect: \(geometry.visibleRect)
+            //              Content offset: \(geometry.contentOffset)
+            //              Container size: \(geometry.containerSize)
+            //              Content size: \(geometry.contentSize)
+            //              At end? \(isAtEnd)
+            //              """)
+            
+            return isAtEnd
+            
+          },
+          action: { _, newValue in
+            isScrolledToEnd(newValue)
+          })
+      
+    } else {
+      
+      content
+    }
+  }
+}
+
 public extension View {
   func isScrollContentClipped(
     isClipped: @escaping (Bool) -> Void
@@ -35,4 +102,11 @@ public extension View {
   }
 }
 
-
+//
+//extension View {
+//  public func isScrolledToEnd(
+//    isScrolledToEnd: @escaping (Bool) -> Void
+//  ) -> some View {
+//    self.modifier(IsScrolledToEnd(isScrolledToEnd: isScrolledToEnd))
+//  }
+//}
