ae953c87bb56e9e8eb0c6d101ca60e61dd14b224
Dave Coleman <dave@dvclmn.com>
2025-05-19 11:12:38 +0200

    Polishes check for is scrolled to the end

--- Sources/BaseHelpers/Scrolling/IsScrolledToEnd.swift
+++ Sources/BaseHelpers/Scrolling/IsScrolledToEnd.swift
@@ -8,48 +8,45 @@
 import SwiftUI
 
 public struct IsScrolledToEnd: ViewModifier {
-  
+
   let isScrolledToEnd: (Bool) -> Void
   public func body(content: Content) -> some View {
     if #available(macOS 15.0, *) {
       content
-        .onScrollGeometryChange(for: Bool.self, of: { geometry in
-          
-          /// How do we figure out if the content is resting at the end?
-          ///
-          /// - We need the full content
-          ///
-          /// Note: 
-          
-          print("""
-            
-            
-            Vis rect: \(geometry.visibleRect)
-            Content offset: \(geometry.contentOffset)
-            Container size: \(geometry.containerSize)
-            Content size: \(geometry.contentSize)
-            
-            Attempted logic: 
-            
-            """)
-          
-          return false
-          
-//          let containerWidth = geometry.containerSize.width
-//          let contentsWidth = geometry.contentSize.width
-//          return containerWidth < contentsWidth
-        }, action: { _, newValue in
-          isScrolledToEnd(newValue)
-        })
-      
+        .onScrollGeometryChange(
+          for: Bool.self,
+          of: { geometry in
+
+            let visibleMaxX = geometry.visibleRect.maxX
+            let contentMaxX = geometry.contentSize.width
+            let tolerance: CGFloat = 1.0
+
+            let isAtEnd = visibleMaxX >= (contentMaxX - tolerance)
+
+//            print(
+//              """
+//              Vis rect: \(geometry.visibleRect)
+//              Content offset: \(geometry.contentOffset)
+//              Container size: \(geometry.containerSize)
+//              Content size: \(geometry.contentSize)
+//              At end? \(isAtEnd)
+//              """)
+
+            return isAtEnd
+
+          },
+          action: { _, newValue in
+            isScrolledToEnd(newValue)
+          })
+
     } else {
-      
+
       content
     }
   }
 }
-public extension View {
-  func isScrolledToEnd(
+extension View {
+  public func isScrolledToEnd(
     isScrolledToEnd: @escaping (Bool) -> Void
   ) -> some View {
     self.modifier(IsScrolledToEnd(isScrolledToEnd: isScrolledToEnd))
