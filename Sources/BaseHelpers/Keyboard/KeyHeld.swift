4b1e6f1350ec1c25b5b75f6e2755a90a68d08318
Dave Coleman <dave@dvclmn.com>
2025-05-25 12:44:16 +0200

    Nice improvements to key pressed, now has...
    ... support for multiple keys to listen for

--- Sources/BaseHelpers/Keyboard/KeyHeld.swift
+++ Sources/BaseHelpers/Keyboard/KeyHeld.swift
@@ -8,41 +8,68 @@
 
 import SwiftUI
 
-public struct KeyHeldModifier: ViewModifier {
+public struct KeysPressedModifier: ViewModifier {
   
-  let key: KeyEquivalent
   @State private var keyDownMonitor: Any?
   @State private var keyUpMonitor: Any?
-  @State private var isKeyCurrentlyPressed: Bool = false
+//  @State private var isKeyCurrentlyPressed: Bool = false
+  @State private var heldKeys: Set<KeyEquivalent> = []
 
-  let isHeld: (Bool) -> Void
-  let onPress: () -> Void
+//  let key: KeyEquivalent
+//  let isHeld: (Bool) -> Void
+//  let onPress: () -> Void
+  
+  let keys: Set<KeyEquivalent>
+  let onPress: (KeyEquivalent) -> Void
+  let isHeld: (KeyEquivalent, Bool) -> Void
   
   public func body(content: Content) -> some View {
     content
       .onAppear {
         keyDownMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
-          if event.characters == String(key.character) {
-            /// Only process if the key isn't already being held down
-            if !isKeyCurrentlyPressed {
-              isKeyCurrentlyPressed = true
-              isHeld(true)
-              onPress()
-            }
-            return nil
-          } else {
+          guard let character = event.characters?.lowercased().first,
+                let key = keys.first(where: { $0.character == character }) else {
             return event
           }
+          
+          if !heldKeys.contains(key) {
+            heldKeys.insert(key)
+            isHeld(key, true)
+            onPress(key)
+          }
+          
+          return nil
+          
+//          if event.characters == String(key.character) {
+//            /// Only process if the key isn't already being held down
+//            if !isKeyCurrentlyPressed {
+//              isKeyCurrentlyPressed = true
+//              isHeld(true)
+//              onPress()
+//            }
+//            return nil
+//          } else {
+//            return event
+//          }
         }
         
         keyUpMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyUp) { event in
-          if event.characters == String(key.character) {
-            isKeyCurrentlyPressed = false
-            isHeld(false)
-            return nil
-          } else {
+          guard let character = event.characters?.lowercased().first,
+                let key = keys.first(where: { $0.character == character }) else {
             return event
           }
+          
+          heldKeys.remove(key)
+          isHeld(key, false)
+          return nil
+          
+//          if event.characters == String(key.character) {
+//            isKeyCurrentlyPressed = false
+//            isHeld(false)
+//            return nil
+//          } else {
+//            return event
+//          }
         }
       }
       .onDisappear {
@@ -58,31 +85,57 @@ public struct KeyHeldModifier: ViewModifier {
 
 // Usage example:
 public extension View {
-  func keyHeld(
-    _ key: KeyEquivalent,
-    isHeld: @escaping (Bool) -> Void
+  func keysPressed(
+    _ keys: Set<KeyEquivalent>,
+    onPress: @escaping (KeyEquivalent) -> Void
   ) -> some View {
     self.modifier(
-      KeyHeldModifier(
-        key: key,
-        isHeld: isHeld,
-        onPress: {}
+      KeysPressedModifier(
+        keys: keys,
+        onPress: onPress,
+        isHeld: { _, _ in }
       )
     )
   }
   
-  func keyPressed(
-    _ key: KeyEquivalent,
-    onPress: @escaping () -> Void
+  func keysHeld(
+    _ keys: Set<KeyEquivalent>,
+    isHeld: @escaping (KeyEquivalent, Bool) -> Void,
+    onPress: @escaping (KeyEquivalent) -> Void = { _ in }
   ) -> some View {
     self.modifier(
-      KeyHeldModifier(
-        key: key,
-        isHeld: { _ in },
-        onPress: onPress
+      KeysPressedModifier(
+        keys: keys,
+        onPress: onPress,
+        isHeld: isHeld
       )
     )
   }
+//  func keyHeld(
+//    _ key: KeyEquivalent,
+//    isHeld: @escaping (Bool) -> Void
+//  ) -> some View {
+//    self.modifier(
+//      KeyHeldModifier(
+//        key: key,
+//        isHeld: isHeld,
+//        onPress: {}
+//      )
+//    )
+//  }
+//  
+//  func keyPressed(
+//    _ key: KeyEquivalent,
+//    onPress: @escaping () -> Void
+//  ) -> some View {
+//    self.modifier(
+//      KeyHeldModifier(
+//        key: key,
+//        isHeld: { _ in },
+//        onPress: onPress
+//      )
+//    )
+//  }
 }
 
 #endif
