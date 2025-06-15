ffb475f6f90f113f6cd4a79414a0642722b45c91
Dave Coleman <dave@dvclmn.com>
2025-06-15 10:24:35 +0200

    Small updates

--- Sources/BaseHelpers/Pointer/PointerChange.swift
+++ Sources/BaseHelpers/Pointer/PointerChange.swift
@@ -11,46 +11,26 @@ import SwiftUI
 
 import SwiftUI
 
-@Observable
-final class PointerHandler {
-  var pointerStyle: NSCursor = .current
-  //  var isWatchingHover: Bool = false
-  var isHovering: Bool = false
-
-  init(
-    pointerStyle: NSCursor
-  ) {
-    self.pointerStyle = pointerStyle
-  }
-}
-
-
 public struct PointerHoverChangeModifier: ViewModifier {
 
-  @State private var pointerHandler: PointerHandler
-  //  @State private var isHovering: Bool = false
-
-  //  let pointerStyle: NSCursor
+  @State private var isHovering: Bool = false
+  let pointerStyle: NSCursor
 
   public init(
     pointerStyle: NSCursor
   ) {
-    self._pointerHandler = State(initialValue: PointerHandler(pointerStyle: pointerStyle))
-    self.pointerHandler = pointerHandler
+    self.pointerStyle = pointerStyle
   }
 
   public func body(content: Content) -> some View {
     content
       .onHover { hover in
-        //        if pointerHandler.isWatchingHover {
-        pointerHandler.isHovering = hover
-        //        }
-
+        isHovering = hover
         DispatchQueue.main.async {
-          if pointerHandler.isHovering {
+          if isHovering {
             // See https://stackoverflow.com/a/62984079/7964697 for details.
             NSApp.windows.forEach { $0.disableCursorRects() }
-            pointerHandler.pointerStyle.push()
+            pointerStyle.push()
 
           } else {
             NSCursor.pop()
@@ -68,33 +48,4 @@ extension View {
   }
 }
 
-public struct PointerChangeModifier: ViewModifier {
-
-  let pointerStyle: NSCursor?
-
-  public func body(content: Content) -> some View {
-    content
-      .task(id: pointerStyle) {
-        DispatchQueue.main.async {
-          if let pointerStyle {
-            // See https://stackoverflow.com/a/62984079/7964697 for details.
-            NSApp.windows.forEach { $0.disableCursorRects() }
-            pointerStyle.push()
-
-          } else {
-            NSCursor.pop()
-            NSApp.windows.forEach { $0.enableCursorRects() }
-          }
-        }
-      }
-
-  }
-}
-extension View {
-  public func pointerChangeOnHover(to style: NSCursor?) -> some View {
-    self.modifier(
-      PointerChangeModifier(pointerStyle: style)
-    )
-  }
-}
 #endif
