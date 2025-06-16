58e8b3692beb20881651968b4696d618b7d8137d
Dave Coleman <dave@dvclmn.com>
2025-06-16 18:17:14 +0200

    Excellent improvements to debounced task modifier

--- Sources/BaseHelpers/Debouncer/DebouncerTaskModifier.swift
+++ Sources/BaseHelpers/Debouncer/DebouncerTaskModifier.swift
@@ -7,51 +7,93 @@
 
 import SwiftUI
 
-public struct DebouncingTaskViewModifier<ID: Equatable>: ViewModifier {
+public struct DebouncedTaskViewModifier<ID: Equatable>: ViewModifier {
+  @State private var debouncer: AsyncDebouncer
   let id: ID
-  let priority: TaskPriority
-  let seconds: TimeInterval
-  let task: @Sendable () async -> Void
+  let interval: TimeInterval
+  let action: () async -> Void
   
   public init(
     id: ID,
-    priority: TaskPriority,
-    seconds: TimeInterval,
-    task: @Sendable @escaping () async -> Void
+    interval: TimeInterval,
+    action: @escaping @Sendable () async -> Void
   ) {
     self.id = id
-    self.priority = priority
-    self.seconds = seconds
-    self.task = task
+    self.interval = interval
+    self._debouncer = State(initialValue: AsyncDebouncer(interval: interval))
+    self.action = action
   }
-  
   public func body(content: Content) -> some View {
-    content.task(id: id, priority: priority) {
-      do {
-        try await Task.sleep(for: .seconds(seconds))
-        await task()
-      } catch {
-        // Ignore cancellation
+    content
+      .task(id: id) {
+        await debouncer.execute { @MainActor in
+          await action()
+        }
       }
-    }
   }
 }
-
-
-public extension View {
-  func task<ID: Equatable>(
+extension View {
+  public func debouncedTask<ID: Equatable>(
     id: ID,
-    priority: TaskPriority = .userInitiated,
-    seconds: TimeInterval = 0.2,
-    task: @Sendable @escaping () async -> Void
+    interval: TimeInterval = 0.2,
+    action: @escaping @Sendable () async -> Void
   ) -> some View {
-    modifier(
-      DebouncingTaskViewModifier(
+    self.modifier(
+      DebouncedTaskViewModifier(
         id: id,
-        priority: priority,
-        seconds: seconds,
-        task: task
+        interval: interval,
+        action: action
       )
     )
   }
 }
+
+//public struct DebouncingTaskViewModifier<ID: Equatable>: ViewModifier {
+//  let id: ID
+//  let priority: TaskPriority
+//  let seconds: TimeInterval
+//  let task: @Sendable () async -> Void
+//  
+//  public init(
+//    id: ID,
+//    priority: TaskPriority,
+//    seconds: TimeInterval,
+//    task: @Sendable @escaping () async -> Void
+//  ) {
+//    self.id = id
+//    self.priority = priority
+//    self.seconds = seconds
+//    self.task = task
+//  }
+//  
+//  public func body(content: Content) -> some View {
+//    content.task(id: id, priority: priority) {
+//      do {
+//        try await Task.sleep(for: .seconds(seconds))
+//        await task()
+//      } catch {
+//        // Ignore cancellation
+//      }
+//    }
+//  }
+//}
+//
+//
+//
+//public extension View {
+//  func task<ID: Equatable>(
+//    id: ID,
+//    priority: TaskPriority = .userInitiated,
+//    seconds: TimeInterval = 0.2,
+//    task: @Sendable @escaping () async -> Void
+//  ) -> some View {
+//    modifier(
+//      DebouncingTaskViewModifier(
+//        id: id,
+//        priority: priority,
+//        seconds: seconds,
+//        task: task
+//      )
+//    )
+//  }
+//}
--- Sources/BaseHelpers/Debouncer/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  BaseHelpers
-//
-//  Created by Dave Coleman on 24/2/2025.
-//
-
-import Foundation
