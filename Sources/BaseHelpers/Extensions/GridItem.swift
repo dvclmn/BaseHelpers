25eb3db9c368e8cdd300c0887d1bf8e59f490273
Dave Coleman <dave@dvclmn.com>
2025-06-29 14:55:50 +0200

    Updates

---
+++ Sources/BaseHelpers/Extensions/GridItem.swift
@@ -0,0 +1,50 @@
+//
+//  GridItem.swift
+//  BaseHelpers
+//
+//  Created by Dave Coleman on 29/6/2025.
+//
+
+import SwiftUI
+
+extension Array where Element == GridItem {
+
+  public static func quickColumns(
+    _ count: Int = 3,
+    spacing: CGFloat? = nil,
+  ) -> Self {
+    let result: Self = Array(
+      repeating: GridItem(.flexible(), spacing: spacing),
+      count: count
+    )
+    return result
+  }
+  
+  public static func quickAdaptiveColumns(
+    min: CGFloat = 20,
+    max: CGFloat = 200,
+    spacing: CGFloat? = nil,
+  ) -> Self {
+    let result = GridItem(
+      .adaptive(minimum: min, maximum: max),
+      spacing: spacing
+    )
+    return [result]
+  }
+  
+//  public static func quickAdaptiveColumns(
+//    min: CGFloat = 20,
+//    max: CGFloat = 200,
+//    spacing: CGFloat? = nil,
+//  ) -> GridItem {
+//    let result = GridItem(
+//      .adaptive(minimum: min, maximum: max),
+//      spacing: spacing
+//    )
+//    return result
+//  }
+}
+
+//extension GridItem {
+//
+//}
---
+++ Sources/BaseHelpers/Extensions/Swatch.swift
@@ -0,0 +1,12 @@
+//
+//  Swatch.swift
+//  BaseHelpers
+//
+//  Created by Dave Coleman on 29/6/2025.
+//
+
+import BaseStyles
+
+extension Swatch {
+  
+}
