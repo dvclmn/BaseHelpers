51abd07ccc2a5e31df54c2a1c4851951e1ecdca3
Dave Coleman <dave@dvclmn.com>
2025-07-04 10:29:13 +0200

    Removes now-unused DisplayString files

--- Sources/BaseHelpers/Extensions/DisplayString.swift
+++ Sources/BaseHelpers/Extensions/DisplayString.swift
@@ -7,17 +7,19 @@
 
 import SwiftUI
 
+/// I think I can just express the other shapes, directly.
+/// E.g. single float values like `CGFloat` using `BinaryFloatingPoint`,
+/// `CGRect`, who has four values to display, etc.
 public protocol ValuePair {
   var valueA: Double { get }
   var valueB: Double { get }
+  
+  var displayString: String { get }
   func displayString(
     _ decimalPlaces: Int,
     grouping: Decimal.FormatStyle.Configuration.Grouping
   ) -> String
 }
-//public protocol ValueSingle{
-//  var value: Double { get }
-//}
 
 extension CGPoint: ValuePair {
   public var valueA: Double { x }
@@ -35,19 +37,8 @@ extension UnitPoint: ValuePair {
   public var valueA: Double { x }
   public var valueB: Double { y }
 }
-//extension BinaryFloatingPoint {
-//  public var value: Double { Double(self) }
-//}
-
-//public protocol ValuePairStringable {
-//  associatedtype Value: ValuePair
-//  var value: Value { get }
-  
-//}
 
 public protocol SingleValue {
-//  associatedtype Value: BinaryFloatingPoint
-//  var value: Value { get }
   func displayString(
     _ decimalPlaces: Int,
     grouping: Decimal.FormatStyle.Configuration.Grouping
@@ -55,6 +46,8 @@ public protocol SingleValue {
 }
 
 extension ValuePair {
+  
+  public var displayString: String { self.displayString() }
   public func displayString(
     _ decimalPlaces: Int = 2,
     grouping: Decimal.FormatStyle.Configuration.Grouping = .automatic
@@ -65,63 +58,3 @@ extension ValuePair {
     return String(formattedA + " x " + formattedB)
   }
 }
-extension BinaryFloatingPoint {
-  public func displayString(
-    _ decimalPlaces: Int = 2,
-    grouping: Decimal.FormatStyle.Configuration.Grouping = .automatic
-  ) -> String {
-    
-    let formatted: String = Double(self).formatted(.number.precision(.fractionLength(decimalPlaces)).grouping(grouping))
-
-    return String(formatted)
-  }
-}
-
-
-
-
-//extension CGPoint: ValuePairable {
-//  public var valueAPath: KeyPath<Self, CGFloat> { \.x }
-//  public var valueBPath: KeyPath<Self, CGFloat> { \.y }
-//}
-//extension CGSize: ValuePairable {
-//  public var valueAPath: KeyPath<Self, CGFloat> { \.width }
-//  public var valueBPath: KeyPath<Self, CGFloat> { \.height }
-//}
-//extension CGVector: ValuePairable {
-//  public var valueAPath: KeyPath<Self, CGFloat> { \.dx }
-//  public var valueBPath: KeyPath<Self, CGFloat> { \.dy }
-//}
-//extension UnitPoint: ValuePairable {
-//  public var valueAPath: KeyPath<Self, CGFloat> { \.x }
-//  public var valueBPath: KeyPath<Self, CGFloat> { \.y }
-//}
-//
-//public protocol ValuePairable {
-//  associatedtype Root: ValuePairable
-//  var valueAPath: KeyPath<Root, CGFloat> { get }
-//  var valueBPath: KeyPath<Root, CGFloat> { get }
-//}
-//public protocol DisplayStringable {
-//  associatedtype Value: ValuePairable
-//  var value: Value { get }
-//  var displayString: String { get }
-//
-//}
-//extension DisplayStringable {
-//  public var displayString: String {
-//    self.displayString()
-//  }
-//
-//  public func displayString(
-//    _ decimalPlaces: Int = 2,
-//    grouping: Decimal.FormatStyle.Configuration.Grouping = .automatic
-//  ) -> String {
-//
-//    let formattedA: String = Double(self[keyPath: valueAPath]).formatted(
-//      .number.precision(.fractionLength(decimalPlaces)).grouping(grouping))
-//    let formattedB: String = Double(self.y).formatted(
-//      .number.precision(.fractionLength(decimalPlaces)).grouping(grouping))
-//    return String(formattedA + " x " + formattedB)
-//  }
-//}
