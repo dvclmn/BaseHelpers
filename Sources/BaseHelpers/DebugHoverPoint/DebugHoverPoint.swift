695043c4dc06b96eb368c43409c067a350d40e36
Dave Coleman <dave@dvclmn.com>
2025-07-04 20:28:31 +0200

    Updates to debug hover point

--- Sources/BaseHelpers/DebugHoverPoint/DebugHoverPoint.swift
+++ Sources/BaseHelpers/DebugHoverPoint/DebugHoverPoint.swift
@@ -10,47 +10,70 @@ import SwiftUI
 public struct DebugHoverPointModifier: ViewModifier {
 
   let point: CGPoint?
+  let containerSize: CGSize
+  private let circleRadius: CGFloat = 18
+
   public func body(content: Content) -> some View {
     content
       .overlay {
         if let point {
-          DebugHoverPointView(point: point)
-            .allowsHitTesting(false)
+          DebugCircle(point)
+
         }
       }
   }
 }
-extension View {
-  public func debugHoverPoint(_ point: CGPoint?) -> some View {
-    self.modifier(DebugHoverPointModifier(point: point))
+extension DebugHoverPointModifier {
+
+//  var pointWasNudged: Bool {
+//    offsetPoint(point) > .zero
+//  }
+
+  var nearbyPoint: UnitPoint? {
+    guard let point else { return nil }
+    return point.nearestAnchor(in: containerSize, centerTolerance: 0.9)
   }
-}
 
-public struct DebugHoverPointView: View {
+  private var edgeBasedOffset: CGSize {
 
-  let point: CGPoint
-  private let circleRadius: CGFloat = 18
-  public var body: some View {
+    guard let nearbyPoint, nearbyPoint != UnitPoint.center else { return .zero }
+    let offset = nearbyPoint.offset(by: 80)
+//    let nudged = point + offset
+//    let nudged = nearbyPoint.applyOffset { axis in
+//      axis == .horizontal ? 10 : 40
+//    } apply: { dx, dy in
+//      CGPoint(x: point.x + dx, y: point.y + dy)
+//    }
 
+    return offset
+  }
+
+  @ViewBuilder
+  func DebugCircle(_ point: CGPoint) -> some View {
     Circle()
       .fill(.brown)
       .frame(width: circleRadius, height: circleRadius)
       .overlay {
         Text(point.displayString)
           .font(.caption.weight(.semibold))
+          .monospaced()
           .foregroundStyle(.secondary)
           .fixedSize()
           .padding(4)
           .background(.thinMaterial)
           .clipShape(.rect(cornerRadius: 3))
           .offset(y: -circleRadius * 2)
+          .offset(edgeBasedOffset)
+          //          .offset(pointWasNudged ? offsetPoint(point) : .zero)
+//          .animation(.spring, value: edgeBasedOffset)
+        //          .animation(.spring, value: pointWasNudged)
       }
       .position(point)
-
+      .allowsHitTesting(false)
   }
 }
-#if DEBUG
-#Preview {
-  DebugHoverPointView(point: CGPoint.quickPreset01)
+extension View {
+  public func debugHoverPoint(_ point: CGPoint?, containerSize: CGSize) -> some View {
+    self.modifier(DebugHoverPointModifier(point: point, containerSize: containerSize))
+  }
 }
-#endif
