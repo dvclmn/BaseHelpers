087cc27182d436573c5e8d3d667e03e8a792a6d4
Dave Coleman <dave@dvclmn.com>
2025-07-09 12:29:16 +0200

    Considering changing approach to keysHeld

--- Sources/BaseHelpers/Extensions/Binding.swift
+++ Sources/BaseHelpers/Extensions/Binding.swift
@@ -7,9 +7,9 @@
 
 import SwiftUI
 
-public extension Binding where Value == CGFloat {
+extension Binding where Value == CGFloat {
   /// Convert `CGFloat` Binding to `Double` Binding
-  var toBindingDouble: Binding<Double> {
+  public var toBindingDouble: Binding<Double> {
     Binding<Double>(
       get: { Double(wrappedValue) },
       set: { wrappedValue = CGFloat($0) }
@@ -17,9 +17,9 @@ public extension Binding where Value == CGFloat {
   }
 }
 
-public extension Binding where Value == Double {
+extension Binding where Value == Double {
   /// Convert `Double` Binding to `CGFloat` Binding
-  var toBindingFloat: Binding<CGFloat> {
+  public var toBindingFloat: Binding<CGFloat> {
     Binding<CGFloat>(
       get: { CGFloat(wrappedValue) },
       set: { wrappedValue = Double($0) }
@@ -27,9 +27,9 @@ public extension Binding where Value == Double {
   }
 }
 
-public extension Binding where Value == Int {
+extension Binding where Value == Int {
   /// Convert `Int` Binding to `Double` Binding
-  var toBindingDouble: Binding<Double> {
+  public var toBindingDouble: Binding<Double> {
     Binding<Double>(
       get: { Double(wrappedValue) },
       set: { wrappedValue = Int($0) }
@@ -37,28 +37,82 @@ public extension Binding where Value == Int {
   }
 }
 
+// MARK: - Binding CGRect
+extension Binding where Value == CGRect {
+  public var getOrigin: CGPoint {
+    return self.wrappedValue.origin
+  }
+  public var getSize: CGSize {
+    return self.wrappedValue.size
+  }
+
+  public var toBindingOrigin: Binding<CGPoint> {
+    return Binding<CGPoint> {
+      return self.wrappedValue.origin
+    } set: { newValue in
+      let currentSize: CGSize = self.wrappedValue.size
+      self.wrappedValue = CGRect(origin: newValue, size: currentSize)
+    }
+  }
+
+  public var toBindingSize: Binding<CGSize> {
+    return Binding<CGSize> {
+      return self.wrappedValue.size
+    } set: { newValue in
+      let currentOrigin: CGPoint = self.wrappedValue.origin
+      self.wrappedValue = CGRect(origin: currentOrigin, size: newValue)
+    }
+  }
+}
+
+// MARK: - Binding CGPoint
+//extension Binding where Value == CGPoint {
+//  public var toBinding: CGPoint {
+//    return self.wrappedValue.origin
+//  }
+//  public var getSize: CGSize {
+//    return self.wrappedValue.size
+//  }
+//  
+//  public var toBindingOrigin: Binding<CGPoint> {
+//    return Binding<CGPoint> {
+//      return self.wrappedValue.origin
+//    } set: { newValue in
+//      let currentSize: CGSize = self.wrappedValue.size
+//      self.wrappedValue = CGRect(origin: newValue, size: currentSize)
+//    }
+//  }
+//  
+//  public var toBindingSize: Binding<CGSize> {
+//    return Binding<CGSize> {
+//      return self.wrappedValue.size
+//    } set: { newValue in
+//      let currentOrigin: CGPoint = self.wrappedValue.origin
+//      self.wrappedValue = CGRect(origin: currentOrigin, size: newValue)
+//    }
+//  }
+//}
 
 //public extension Binding {
-//  
+//
 //  static func convert<TInt, TFloat>(_ intBinding: Binding<TInt>) -> Binding<TFloat>
 //  where TInt: BinaryInteger & Sendable,
 //        TFloat: BinaryFloatingPoint, TFloat.Stride: BinaryFloatingPoint {
-//          
+//
 //          Binding<TFloat> (
 //            get: { TFloat(intBinding.wrappedValue) },
 //            set: { intBinding.wrappedValue = TInt($0) }
 //          )
 //        }
-//  
+//
 //  static func convert<TFloat, TInt>(_ floatBinding: Binding<TFloat>) -> Binding<TInt>
 //  where TFloat: BinaryFloatingPoint & Sendable,
 //        TFloat.Stride: BinaryFloatingPoint,
 //        TInt: BinaryInteger {
-//          
+//
 //          Binding<TInt> (
 //            get: { TInt(floatBinding.wrappedValue) },
 //            set: { floatBinding.wrappedValue = TFloat($0) }
 //          )
 //        }
 //}
-
---
+++ Sources/BaseHelpers/Grid/Model+GridRect.swift
@@ -0,0 +1,58 @@
+//
+//  Model+GridRect.swift
+//  BaseHelpers
+//
+//  Created by Dave Coleman on 8/7/2025.
+//
+
+import Foundation
+
+public struct GridRect: GridBase {
+  public let origin: GridPosition
+  public let size: GridDimensions
+  
+  public init(origin: GridPosition, size: GridDimensions) {
+    self.origin = origin
+    self.size = size
+  }
+  
+  /// Creates a GridRect that bounds two GridPositions inclusively
+  public init(bounding a: GridPosition, _ b: GridPosition) {
+    let minRow = min(a.row, b.row)
+    let maxRow = max(a.row, b.row)
+    let minCol = min(a.column, b.column)
+    let maxCol = max(a.column, b.column)
+    
+    self.origin = GridPosition(row: minRow, column: minCol)
+    self.size = GridDimensions(
+      columns: maxCol - minCol + 1,
+      rows: maxRow - minRow + 1
+    )
+  }
+}
+
+extension GridRect {
+  /// Iterates over all GridPositions within this GridRect
+  public var allPositions: [GridPosition] {
+    (0..<size.rows).flatMap { r in
+      (0..<size.columns).map { c in
+        GridPosition(row: origin.row + r, column: origin.column + c)
+      }
+    }
+  }
+  
+  public func contains(_ position: GridPosition) -> Bool {
+    let rowRange = origin.row..<(origin.row + size.rows)
+    let colRange = origin.column..<(origin.column + size.columns)
+    return rowRange.contains(position.row) && colRange.contains(position.column)
+  }
+  
+  func asCGRect(cellSize: CGSize) -> CGRect {
+    CGRect(
+      x: CGFloat(origin.column) * cellSize.width,
+      y: CGFloat(origin.row) * cellSize.height,
+      width: CGFloat(size.columns) * cellSize.width,
+      height: CGFloat(size.rows) * cellSize.height
+    )
+  }
+}
