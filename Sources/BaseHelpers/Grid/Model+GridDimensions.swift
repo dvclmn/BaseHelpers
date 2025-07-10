4f6bacdac9c87fdacecb9c7a068c353798b45564
Dave Coleman <dave@dvclmn.com>
2025-07-10 16:46:23 +0200

    Ongoing improvements to Grid models

--- Sources/BaseHelpers/Grid/Model+GridDimensions.swift
+++ Sources/BaseHelpers/Grid/Model+GridDimensions.swift
@@ -10,18 +10,27 @@ import Foundation
 public struct GridDimensions: GridBase {
   public var columns: Int
   public var rows: Int
-  
+
   public static let zero = GridDimensions(columns: 0, rows: 0)
 
   public init(columns: Int, rows: Int) {
     self.columns = columns
     self.rows = rows
   }
-  
-  public init(size: CGSize, cellSize: CGSize) {
-    let widthInCells = max(0, Int(size.width / cellSize.width))
-    let heightInCells = max(0, Int(size.height / cellSize.height))
-    self.init(columns: widthInCells, rows: heightInCells)
+
+  public init(
+    size: CGSize,
+    cellSize: CGSize
+  ) {
+
+    let widthInCells = Int(size.width / cellSize.width)
+    let heightInCells = Int(size.height / cellSize.height)
+
+    /// This means we at least initialise with 1 Cell
+    let widthClamped = max(1, widthInCells)
+    let heightClamped = max(1, heightInCells)
+
+    self.init(columns: widthClamped, rows: heightClamped)
   }
 }
 
@@ -31,11 +40,9 @@ extension GridDimensions {
 
   public var respectsMaxCells: Bool {
     return self.bothLessThan(rhs: Self.maxCellsAlongLength)
-//    return self < Self.maxCellsAlongLength
   }
   public var respectsMinCells: Bool {
     return self.bothGreaterThan(rhs: Self.minCellsAlongLength)
-//    return self > Self.minCellsAlongLength
   }
 
   public func contains(_ position: GridPosition) -> Bool {
@@ -47,14 +54,14 @@ extension GridDimensions {
 }
 
 extension GridDimensions {
-  
+  // MARK: - Greater than
   public func bothGreaterThan(rhs: GridDimensions) -> Bool {
     self.rows > rhs.rows && self.columns > rhs.columns
   }
   public func bothGreaterThan(rhs: Int) -> Bool {
     self.rows > rhs && self.columns > rhs
   }
-  
+
   // MARK: - Less than
   public func bothLessThan(rhs: GridDimensions) -> Bool {
     self.rows < rhs.rows && self.columns < rhs.columns
@@ -63,22 +70,6 @@ extension GridDimensions {
     self.rows < rhs && self.columns < rhs
   }
 }
-// MARK: - Greater than
-//public func > (lhs: GridDimensions, rhs: GridDimensions) -> Bool {
-//  lhs.rows > rhs.rows || lhs.columns > rhs.columns
-//}
-//public func > (lhs: GridDimensions, rhs: Int) -> Bool {
-//  lhs.rows > rhs || lhs.columns > rhs
-//}
-//
-//// MARK: - Less than
-//public func < (lhs: GridDimensions, rhs: GridDimensions) -> Bool {
-//  lhs.rows < rhs.rows || lhs.columns < rhs.columns
-//}
-//public func < (lhs: GridDimensions, rhs: Int) -> Bool {
-//  lhs.rows < rhs || lhs.columns < rhs
-//}
-
 
 extension GridDimensions: CustomStringConvertible {
   public var description: String {
--- Sources/BaseHelpers/Grid/Model+GridRect.swift
+++ Sources/BaseHelpers/Grid/Model+GridRect.swift
@@ -10,61 +10,32 @@ import Foundation
 public struct GridRect: GridBase {
   public let origin: GridPosition
   public let size: GridDimensions
-  let cellSize: CGSize
-
-  /// This expects a `CGRect` that's already mapped
-  public init(
-    rect: CGRect,
-    cellSize: CGSize
-  ) {
-
-    assert(rect.width >= 0 && rect.height >= 0, "GridRect initialised with negative dimensions: \(rect)")
-
-    let origin = GridPosition(
-      point: rect.origin,
-      cellSize: cellSize
-    )
-    let size = GridDimensions(size: rect.size, cellSize: cellSize)
-
-    self.init(origin: origin, size: size, cellSize: cellSize)
-  }
 
   public init(
     origin: GridPosition,
     size: GridDimensions,
-    cellSize: CGSize
   ) {
     self.origin = origin
     self.size = size
-    self.cellSize = cellSize
   }
-
-  //  public static func gridRect(
-  //    for rect: CGRect,
-  //    cellSize: CGSize
-  //  ) -> GridRect {
-  //    let topLeft = position(at: rect.origin, cellSize: cellSize)
-  //    let bottomRight = position(at: CGPoint(x: rect.maxX, y: rect.maxY), cellSize: cellSize)
-  //    let width = bottomRight.column - topLeft.column + 1
-  //    let height = bottomRight.row - topLeft.row + 1
-  //    return GridRect(origin: topLeft, size: GridDimensions(columns: width, rows: height))
-  //  }
 
   /// Creates a GridRect that bounds two GridPositions inclusively
   public init(
     bounding a: GridPosition,
-    _ b: GridPosition
+    _ b: GridPosition,
   ) {
     let minRow = min(a.row, b.row)
     let maxRow = max(a.row, b.row)
     let minCol = min(a.column, b.column)
     let maxCol = max(a.column, b.column)
 
-    self.origin = GridPosition(row: minRow, column: minCol)
-    self.size = GridDimensions(
+    let origin = GridPosition(row: minRow, column: minCol)
+    let size = GridDimensions(
       columns: maxCol - minCol + 1,
       rows: maxRow - minRow + 1
     )
+    self.init(origin: origin, size: size)
+
   }
 }
 
@@ -79,18 +50,29 @@ extension GridRect {
     }
   }
 
-  //  public var allPositions: [GridPosition] {
-  //    (0..<size.rows).flatMap { r in
-  //      (0..<size.columns).map { c in
-  //        GridPosition(row: origin.row + r, column: origin.column + c)
-  //      }
-  //    }
-  //  }
-
   public func contains(_ position: GridPosition) -> Bool {
     let rowRange = origin.row..<(origin.row + size.rows)
     let colRange = origin.column..<(origin.column + size.columns)
     return rowRange.contains(position.row) && colRange.contains(position.column)
+  }
+
+  public static func cgRectSnappedToCell(
+    rect: CGRect,
+    cellSize: CGSize
+  ) -> CGRect {
+    assert(rect.width >= 0 && rect.height >= 0, "GridRect initialised with negative dimensions: \(rect)")
+
+    let origin = GridPosition(
+      point: rect.origin,
+      cellSize: cellSize
+    )
+    
+    let size = GridDimensions(size: rect.size, cellSize: cellSize)
+
+    let gridRect = GridRect(origin: origin, size: size)
+    let cgRect = gridRect.asCGRect(cellSize: cellSize)
+    return cgRect
+
   }
 
   public func asCGRect(cellSize: CGSize) -> CGRect {
@@ -103,6 +85,7 @@ extension GridRect {
   }
 
   public func clamped(to bounds: GridDimensions) -> GridRect {
+
     let clampedOrigin = GridPosition(
       row: max(0, min(origin.row, bounds.rows - 1)),
       column: max(0, min(origin.column, bounds.columns - 1))
@@ -116,6 +99,9 @@ extension GridRect {
       rows: max(0, endRow - clampedOrigin.row)
     )
 
-    return GridRect(origin: clampedOrigin, size: clampedSize)
+    return GridRect(
+      origin: clampedOrigin,
+      size: clampedSize,
+    )
   }
 }
