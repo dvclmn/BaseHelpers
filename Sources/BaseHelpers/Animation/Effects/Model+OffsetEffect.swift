5987cc918f13255053cd1e1bf7d4524b8be596eb
Dave Coleman <dave@dvclmn.com>
2025-08-20 11:33:59 +1000

    Updates

--- Sources/BaseHelpers/Animation/Effects/Model+OffsetEffect.swift
+++ Sources/BaseHelpers/Animation/Effects/Model+OffsetEffect.swift
@@ -11,86 +11,121 @@ import SwiftUI
 //public struct Effect: Documentable {
 //  var axisA: SmoothedProperty
 //  var axisB: SmoothedProperty
-//  
-//  
+//
+//
 //}
 
-public protocol WaveOutput: Sendable, Equatable {}
+public protocol WaveOutput: Sendable, Codable, Equatable {
+  static func defaultValue(for): Self { get }
+}
 
 public typealias WaveTransform<T> = @Sendable (CGFloat, CGFloat) -> T
 
 /// A generic property whose value is driven by a Wave.
-public struct WaveDrivenProperty<T: WaveOutput>: Documentable {
+public struct Effect<T: WaveOutput>: Documentable {
+  
+  public static var kind: EffectKind
   /// The base wave value is always a scalar between -1...1
-//  var wave: Wave
-//  public var wave: S
+  //  var wave: Wave
+  //  public var wave: S
   public var waveComposition: WaveComposition
-  
+
   /// How to transform the wave’s scalar output into the effect’s Value.
-  private var transform: WaveTransform<T>
-//  private var transform: (CGFloat) -> Value
-  
+  private var transform: WaveTransform<T> = { _, _ in return T.defaultValue}
+  //  private var transform: (CGFloat) -> Value
+
   /// Optional: base value around which the wave oscillates.
-//  var base: T
-  
+    var intensity: T
+
   enum CodingKeys: String, CodingKey {
-//    case base
+        case base
     case waveComposition
   }
-  
+
   public init(
-//    base: T,
+    kind: EffectKind,
+    intensity: T,
     waveComposition: WaveComposition,
     transform: @escaping WaveTransform<T>
   ) {
-//    self.base = base
+    self.kind = kind
+    self.intensity = intensity
     self.waveComposition = waveComposition
     self.transform = transform
   }
-  
+
   /// Returns the current value given a normalised wave position (0...1)
   public func value(elapsed: CGFloat) -> T {
-    let waveScalar = wave.value(elapsed: elapsed) // CGFloat -1...1
+    let waveScalar = wave.value(elapsed: elapsed)  // CGFloat -1...1
     return transform(waveScalar)
   }
-  
+
   // Mutating helpers for bindings
-//  mutating func setBase(_ newBase: T) {
-//    base = newBase
-//  }
-  
+  //  mutating func setBase(_ newBase: T) {
+  //    base = newBase
+  //  }
+
   public mutating func updateWave(_ newWave: Wave) {
     wave = newWave
   }
 }
 
-extension CGFloat: WaveOutput {}
+extension CGFloat: WaveOutput {
+  static var defaultValue: CGFloat
+}
 extension CGSize: WaveOutput {}
 extension Angle: WaveOutput {}
 
 extension WaveDrivenProperty where T == CGFloat {
-  public static func scalar(amplitude: CGFloat, wave: Wave) -> Self {
-    Self(wave: wave) { scalar in
+  public static func scalar(
+    kind: EffectKind,
+    intensity: CGFloat,
+    amplitude: CGFloat,
+    waveComposition: WaveComposition,
+  ) -> Self {
+    Self(
+      kind: kind,
+      intensity: intensity,
+      waveComposition: waveComposition
+      ) { axisA, axisB in
       scalar * amplitude
     }
   }
 }
 
 extension WaveDrivenProperty where T == CGSize {
-  public static func size(amplitude: CGSize, wave: Wave) -> Self {
-    Self(wave: wave) { scalar in
+  public static func size(
+    kind: EffectKind,
+    intensity: CGSize,
+    amplitude: CGSize,
+    waveComposition: WaveComposition,
+  ) -> Self {
+    Self(
+      kind: kind,
+      intensity: intensity,
+      waveComposition: waveComposition
+    ) { axisA, axisB in
       CGSize(
-        width: scalar * amplitude.width,
-        height: scalar * amplitude.height
+        width: intensity + axisA * amplitude.width,
+        height: intensity + axisB * amplitude.height
       )
     }
   }
 }
 
 extension WaveDrivenProperty where T == Angle {
-  public static func angle(amplitude: Angle, wave: Wave) -> Self {
-    Self(wave: wave) { scalar in
-      Angle(degrees: scalar * amplitude.degrees)
+  public static func angle(
+    kind: EffectKind,
+    intensity: Angle,
+    amplitude: Angle,
+    waveComposition: WaveComposition,
+  ) -> Self {
+    Self(
+      kind: kind,
+      intensity: intensity,
+      waveComposition: waveComposition
+    ) { axisA, axisB in
+      Angle(degrees: intensity + axisA * amplitude.degrees)
     }
   }
 }
@@ -98,7 +133,7 @@ extension WaveDrivenProperty where T == Angle {
 //public struct OffsetEffect: AnimatableEffect {
 //
 //  var offset: WaveDrivenProperty<CGSize>
-//  
+//
 ////  public typealias Intensity = CGSize
 ////  public static var defaultIntensity: Intensity { Intensity.zero }
 ////
--- Sources/BaseHelpers/Extensions/Angle.swift
+++ Sources/BaseHelpers/Extensions/Angle.swift
@@ -12,3 +12,19 @@ extension Angle {
     Angle(radians: lhs.radians * Double(rhs))
   }
 }
+
+extension Angle: @retroactive Decodable {}
+extension Angle: @retroactive Encodable {}
+
+extension Angle {
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(radians) // Always store radians for consistency
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let radians = try container.decode(Double.self)
+    self.init(radians: radians)
+  }
+}
