9b89f038919e29e8f12540249e58b99bfd701fef
Dave Coleman <dave@dvclmn.com>
2025-08-23 10:33:10 +1000

    Colour names update

--- Sources/BaseHelpers/CustomColour/Swatch+Generated.swift
+++ Sources/BaseHelpers/CustomColour/Swatch+Generated.swift
@@ -1,103 +1,95 @@
 // swiftlint:disable all
 // Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
-
 #if os(macOS)
-  import AppKit
+import AppKit
 #elseif os(iOS)
-  import UIKit
+import UIKit
 #elseif os(tvOS) || os(watchOS)
-  import UIKit
+import UIKit
 #endif
 #if canImport(SwiftUI)
-  import SwiftUI
+import SwiftUI
 #endif
 
-
-
-
-
-
-  public enum Swatch: String, Codable, CaseIterable, Identifiable, Sendable, Hashable, Equatable {
-      case asciiBlack
-      case asciiBlue
-      case asciiBrown
-      case asciiGreen
-      case asciiGrey
-      case asciiMaroon
-      case asciiPurple
-      case asciiRed
-      case asciiTeal
-      case asciiTealDull
-      case asciiWarmWhite
-      case asciiWhite
-      case asciiYellow
-      case blackTrue
-      case blue10
-      case blue20
-      case blue30
-      case blue40
-      case blue50
-      case blue60
-      case brown30
-      case brown40
-      case brown50
-      case brown60
-      case green20
-      case green30V
-      case green50
-      case green70
-      case grey05
-      case grey10
-      case grey20
-      case grey30
-      case grey40
-      case grey80
-      case grey90
-      case lime30
-      case lime40
-      case neonGreen
-      case neonOrange
-      case neonPink
-      case neonYellow
-      case olive40
-      case olive70
-      case orange30
-      case peach20
-      case peach30
-      case peach30V
-      case peach40
-      case plum30
-      case plum40
-      case plum50
-      case plum60
-      case plum70
-      case plum80
-      case plum90
-      case purple20
-      case purple30
-      case purple40
-      case purple50
-      case purple70
-      case red50V
-      case slate30
-      case slate40
-      case slate50
-      case slate60
-      case slate70
-      case slate80
-      case slate90
-      case teal30
-      case teal50
-      case terracotta30
-      case terracotta40
-      case terracotta50
-      case terracotta60
-      case whiteBone
-      case whiteOff
-      case whiteTrue
-      case yellow30
-      case yellow40
-  }
-
-
+public enum Swatch: String, Codable, CaseIterable, Identifiable, Sendable, Hashable, Equatable {
+  case asciiBlack
+  case asciiBlue
+  case asciiBrown
+  case asciiGreen
+  case asciiGrey
+  case asciiMaroon
+  case asciiPurple
+  case asciiRed
+  case asciiTeal
+  case asciiTealDull
+  case asciiWarmWhite
+  case asciiWhite
+  case asciiYellow
+  case blackTrue
+  case blue10
+  case blue20
+  case blue30
+  case blue40
+  case blue50
+  case blue60
+  case brown30
+  case brown40
+  case brown50
+  case brown60
+  case green20
+  case green30V
+  case green50
+  case green70
+  case grey05
+  case grey10
+  case grey20
+  case grey30
+  case grey40
+  case grey80
+  case grey90
+  case lime30
+  case lime40
+  case neonGreen
+  case neonOrange
+  case neonPink
+  case neonYellow
+  case olive40
+  case olive70
+  case orange30
+  case peach20
+  case peach30
+  case peach30V
+  case peach40
+  case plum30
+  case plum40
+  case plum50
+  case plum60
+  case plum70
+  case plum80
+  case plum90
+  case purple20
+  case purple30
+  case purple40
+  case purple50
+  case purple70
+  case red50V
+  case slate30
+  case slate40
+  case slate50
+  case slate60
+  case slate70
+  case slate80
+  case slate90
+  case teal30
+  case teal50
+  case terracotta30
+  case terracotta40
+  case terracotta50
+  case terracotta60
+  case whiteBone
+  case whiteOff
+  case whiteTrue
+  case yellow30
+  case yellow40
+}
