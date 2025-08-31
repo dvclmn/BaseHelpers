e8d6616910dff1e68b23f7454cf708db2310eec6
Dave Coleman <dave@dvclmn.com>
2025-08-31 12:26:33 +1000

    Moves more colour code here from BaseComponents

--- Sources/BaseHelpers/Icons/CustomSymbols.swift
+++ Sources/BaseHelpers/Icons/CustomSymbols.swift
@@ -5,12 +5,12 @@
 //  Created by Dave Coleman on 11/7/2025.
 //
 
-import Foundation
-
-
-
-public enum BaseStylesResources {
-  public static let bundle: Bundle = {
-    Bundle.module
-  }()
-}
+//import Foundation
+//
+//
+//
+//public enum BaseStylesResources {
+//  public static let bundle: Bundle = {
+//    Bundle.module
+//  }()
+//}
