75821863cc7e60eda7d4deeaf1f0024291c402b5
Dave Coleman <dave@dvclmn.com>
2025-09-13 13:48:37 +0200

    Refines keyboard shortcut display strings

--- Sources/BaseHelpers/Extensions/Keyboard/EventModifiers.swift
+++ Sources/BaseHelpers/Extensions/Keyboard/EventModifiers.swift
@@ -9,34 +9,6 @@ import SwiftUI
 
 extension EventModifiers {
 
-  //  var name: String {
-  //    switch self {
-  //      case .all: "All"
-  //      case .command: "Command"
-  //      case .option: "Option"
-  //      case .shift: "Shift"
-  //      case .control: "Control"
-  //      case .capsLock: "Caps Lock"
-  //      case .numericPad: "Numeric Pad"
-  //      default: "Unknown"
-  //    }
-  //  }
-  //
-  //  var symbolLiteral: Character {
-  //    switch self {
-  //      case .shift: "􀆝"
-  //      case .option: "􀆕"
-  //      case .command: "􀆔"
-  //      case .control: "􀆍"
-  //      case .capsLock: "􀆡"
-  //      default: "?"
-  //    }
-  //  }
-
-  //  var nameAndSymbol: String {
-  //    "\(self.name) (\(self.symbolLiteral))"
-  //  }
-
   public struct ModifierSymbol {
     let name: String
     let symbol: Character
@@ -49,7 +21,7 @@ extension EventModifiers {
       (.option, .init(name: "Option", symbol: "􀆕")),
       (.command, .init(name: "Command", symbol: "􀆔")),
       (.capsLock, .init(name: "Caps Lock", symbol: "􀆡")),
-      //      (.numericPad, .init(name: "Numeric Pad", symbol: "􀅱")),
+      (.numericPad, .init(name: "Numeric Pad", symbol: "􀅱")),
     ]
   }
 
@@ -79,130 +51,4 @@ extension EventModifiers {
     return "\(names) (\(symbols))"
   }
 
-  //  var nameAndSymbol: String {
-  //    let active = activeModifiers
-  //    if active.isEmpty {
-  //      return "None"
-  //    }
-  //    return active
-  //      .map { "\($0.name) (\($0.symbol))" }
-  //      .joined(separator: " + ")
-  //  }
 }
-
-//  enum Key: ShortcutKey, CaseIterable {
-//    static public let allCases: [KBShortcut.Key] = [
-//      .character("a"),
-//      .clear,
-//      .delete,
-//      .deleteForward,
-//      .upArrow,
-//      .downArrow,
-//      .leftArrow,
-//      .rightArrow,
-//      .escape,
-//      .home,
-//      .end,
-//      .pageUp,
-//      .pageDown,
-//      .`return`,
-//      .space,
-//      .tab,
-//    ]
-//
-//    case character(Character)
-//    case clear
-//    case delete
-//    case deleteForward
-//    case upArrow
-//    case downArrow
-//    case leftArrow
-//    case rightArrow
-//    case escape
-//    case home
-//    case end
-//    case pageUp
-//    case pageDown
-//    case `return`
-//    case space
-//    case tab
-//
-//    public var name: String {
-//      switch self {
-//        case .character(let character): "\(character.lowercased())"
-//        case .clear: "Clear"
-//        case .delete: "Delete"
-//        case .deleteForward: "Delete Forward"
-//        case .upArrow: "Up Arrow"
-//        case .downArrow: "Down Arrow"
-//        case .leftArrow: "Left Arrow"
-//        case .rightArrow: "Right Arrow"
-//        case .escape: "Escape"
-//        case .home: "Home"
-//        case .end: "End"
-//        case .pageUp: "Page Up"
-//        case .pageDown: "Page Down"
-//        case .`return`: "Return"
-//        case .space: "Space"
-//        case .tab: "Tab"
-//
-//      }
-//    }
-//
-//    public var symbolName: String {
-//      switch self {
-//        case .character(let character): "\(character)"
-//        case .clear: "xmark"
-//        case .delete: "delete.backward"
-//        case .deleteForward: "delete.forward"
-//        case .upArrow: "arrowtriangle.up"
-//        case .downArrow: "arrowtriangle.down"
-//        case .leftArrow: "arrowtriangle.left"
-//        case .rightArrow: "arrowtriangle.right"
-//        case .escape: "escape"
-//        case .home: "arrow.up.left"
-//        case .end: "arrow.down.right"
-//        case .pageUp: "arrow.up.to.line.compact"
-//        case .pageDown: "arrow.down.to.line.compact"
-//        case .`return`: "return"
-//        case .space: "space"
-//        case .tab: "arrow.forward.to.line"
-//
-//      }
-//    }
-//
-//    public var symbolLiteral: Character {
-//      switch self {
-//        case .character(let character): character
-//        case .clear: "􀆄"
-//        case .delete: "􁂈"
-//        case .deleteForward: "􁂒"
-//        case .upArrow: "􀄤"
-//        case .downArrow: "􀄥"
-//        case .leftArrow: "􀄦"
-//        case .rightArrow: "􀰇"
-//        case .escape: "􀆧"
-//        case .home: "􀰹"
-//        case .end: "􀱈"
-//        case .pageUp: "􀅃"
-//        case .pageDown: "􀅄"
-//        case .`return`: "􀅇"
-//        case .space: "􁁺"
-//        case .tab: "􁂎"
-//      }
-//    }
-//
-//    public var alternativeLiteral: Character? {
-//      switch self {
-//        case .upArrow: "↑"
-//        case .downArrow: "↓"
-//        case .leftArrow: "←"
-//        case .rightArrow: "→"
-//        default: nil
-//      }
-//    }
-//
-//    public var type: KBShortcut.KeyType {
-//      .key
-//    }
-//
--- Sources/BaseHelpers/Extensions/Keyboard/KeyboardShortcut.swift
+++ Sources/BaseHelpers/Extensions/Keyboard/KeyboardShortcut.swift
@@ -5,3 +5,12 @@
 //  Created by Dave Coleman on 16/6/2025.
 //
 
+import SwiftUI
+
+extension KeyboardShortcut {
+  public var displayString: String {
+    let modifiers: String = self.modifiers.symbols
+    let key: String = self.key.symbolLiteral.toString.uppercased()
+    return modifiers + key
+  }
+}
