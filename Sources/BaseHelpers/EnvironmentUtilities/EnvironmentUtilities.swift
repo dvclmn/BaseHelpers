20c89cf8086c92d40884e454d18dcd0e64dfab69
Dave Coleman <dave@dvclmn.com>
2025-09-21 18:00:43 +0200

    Updates

--- Sources/BaseHelpers/EnvironmentUtilities/EnvironmentUtilities.swift
+++ Sources/BaseHelpers/EnvironmentUtilities/EnvironmentUtilities.swift
@@ -14,42 +14,41 @@ extension EnvironmentValues {
   ///
   /// I guess like in a graphics app, so maybe this is a bit domain-specific
   @Entry public var viewportSize: CGSize? = nil
-  
+
   @Entry public var isPreviewOnlyDebugMode: Bool = false
 
   @Entry public var isEmphasised: Bool = false
   @Entry public var isCompactMode: Bool = false
   @Entry public var isConnectedToInternet: Bool = false
   @Entry public var isScrollAtStart: Bool = true
-  
-//  @Entry public var networkMonitor: NetworkMonitor = .init()
 
 }
 
-
-public enum CompactReductionStrength: CGFloat {
-  case slight = 0.95
-  case moderate = 0.85
-  case standard = 0.7
-  case something = 0.6
+/// Where `1.0` is original size, and reduction goes down from there
+public enum ReductionStrength: CGFloat {
+  case weeBit = 0.95
+  case slight = 0.85
+  case moderate = 0.7
+  case standard = 0.6
   case strong = 0.45
+  case veryStrong = 0.3
 }
 
 extension Bool {
-  public func compacted<T: BinaryFloatingPoint>(
+  public func compact<T: BinaryFloatingPoint>(
     _ value: T,
-//    _ isCompact: Bool,
-    strength: CompactReductionStrength
+    by strength: ReductionStrength = .standard
   ) -> T {
-    return self ? (value * T(strength.rawValue)) : value
+    let reduced = value * T(strength.rawValue)
+    return self ? reduced : value
   }
 }
 
-extension BinaryFloatingPoint {
-  public func compacted(
-    _ isCompact: Bool,
-    strength: CompactReductionStrength
-  ) -> Self {
-    return isCompact ? self * Self(strength.rawValue) : self
-  }
-}
+//extension BinaryFloatingPoint {
+//  public func compacted(
+//    _ isCompact: Bool,
+//    strength: ReductionStrength
+//  ) -> Self {
+//    return isCompact ? self * Self(strength.rawValue) : self
+//  }
+//}
