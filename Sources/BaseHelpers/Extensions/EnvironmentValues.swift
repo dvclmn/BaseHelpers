b1f7a244b2b77bdc211804778651b739bbb31128
Dave Coleman <dave@dvclmn.com>
2025-09-22 13:27:56 +0200

    Moves Env Values over to Extensions folder

--- Sources/BaseHelpers/Extensions/EnvironmentValues.swift
+++ Sources/BaseHelpers/Extensions/EnvironmentValues.swift
@@ -1,22 +1,22 @@
 //
-//  EnvironmentUtilities.swift
+//  EnvironmentValues.swift
 //  BaseHelpers
 //
-//  Created by Dave Coleman on 24/6/2025.
+//  Created by Dave Coleman on 22/9/2025.
 //
 
 import SwiftUI
 
 extension EnvironmentValues {
-
+  
   /// This is expected to represent a single, self-contained View,
   /// not including any native components like Toolbar or Inspector.
   ///
   /// I guess like in a graphics app, so maybe this is a bit domain-specific
   @Entry public var viewportSize: CGSize? = nil
-
+  
   @Entry public var isPreviewOnlyDebugMode: Bool = false
-
+  
   @Entry public var isEmphasised: Bool = false
   
   /// This is handy to have, for basic needs, but at times I need
@@ -26,7 +26,7 @@ extension EnvironmentValues {
   
   @Entry public var isConnectedToInternet: Bool = false
   @Entry public var isScrollAtStart: Bool = true
-
+  
 }
 
 //extension BinaryFloatingPoint {
