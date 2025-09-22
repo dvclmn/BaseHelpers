e1c2133c513da9b027be1913eb57fee4c3de69a6
Dave Coleman <dave@dvclmn.com>
2025-09-22 12:27:32 +0200

    Further updates to Animal

--- Sources/BaseHelpers/DummyContent/Animal.swift
+++ Sources/BaseHelpers/DummyContent/Animal.swift
@@ -30,15 +30,49 @@ extension DummyContent {
 extension DummyContent.Animal {
 
   public static let data = [
-    Self(QuickLabel("Giraffe", icon: .emoji("🐶")), state: true, category: Category.cute),
-    Self(QuickLabel("Tapir", icon: .emoji("🐶")), state: false, category: Category.predator),
-    Self(QuickLabel("Dog", icon: .emoji("🐶")), state: true, category: Category.endangered),
-    Self(QuickLabel("Cat", icon: .emoji("🐶")), state: false, category: Category.cute),
-    Self(QuickLabel("Orangutan", icon: .emoji("🐶")), state: false, category: Category.extinct),
-    Self(QuickLabel("Bearded Dragon", icon: .emoji("🐶")), state: false, category: Category.predator),
-    Self(QuickLabel("Goat", icon: .emoji("🐶")), state: false, category: Category.cute),
-    Self(QuickLabel("Ibis", icon: .emoji("🐶")), state: true, category: Category.extinct),
-    Self(QuickLabel("Zebra", icon: .emoji("🐶")), state: false, category: Category.endangered),
+    Self("Giraffe", state: true, category: Category.cute),
+    Self("Tapir", state: false, category: Category.predator),
+    Self("Dog", state: true, category: Category.endangered),
+    Self("Cat", state: false, category: Category.cute),
+    Self("Orangutan", state: false, category: Category.extinct),
+    Self("Bearded Dragon", state: false, category: Category.predator),
+    Self("Goat", state: false, category: Category.cute),
+    Self("Ibis", state: true, category: Category.extinct),
+    Self("Zebra", state: false, category: Category.endangered),
+    //    Self(QuickLabel("Giraffe", icon: .emoji("🐶")), state: true, category: Category.cute),
+    //    Self(QuickLabel("Tapir", icon: .emoji("🐶")), state: false, category: Category.predator),
+    //    Self(QuickLabel("Dog", icon: .emoji("🐶")), state: true, category: Category.endangered),
+    //    Self(QuickLabel("Cat", icon: .emoji("🐶")), state: false, category: Category.cute),
+    //    Self(QuickLabel("Orangutan", icon: .emoji("🐶")), state: false, category: Category.extinct),
+    //    Self(QuickLabel("Bearded Dragon", icon: .emoji("🐶")), state: false, category: Category.predator),
+    //    Self(QuickLabel("Goat", icon: .emoji("🐶")), state: false, category: Category.cute),
+    //    Self(QuickLabel("Ibis", icon: .emoji("🐶")), state: true, category: Category.extinct),
+    //    Self(QuickLabel("Zebra", icon: .emoji("🐶")), state: false, category: Category.endangered),
+  ]
+
+  public static let iconSymbol: [IconLiteral] = [
+    .symbol(Icons.binoculars.icon),
+    .symbol(Icons.window.icon),
+    .symbol(Icons.star.icon),
+    .symbol(Icons.folder.icon),
+    .symbol(Icons.trophy.icon),
+    .symbol(Icons.trash.icon),
+    .symbol(Icons.bookmark.icon),
+    .symbol(Icons.rpg.icon),
+    .symbol(Icons.rotate.icon),
+  ]
+
+  static let iconEmoji: [IconLiteral] = [
+    .emoji("🐶"),
+    .emoji("🐱"),
+    .emoji("🐭"),
+    .emoji("🐹"),
+    .emoji("🐰"),
+    .emoji("🦎"),
+    .emoji("🦖"),
+    .emoji("🦗"),
+    .emoji("🦘"),
+    //    .emoji("🦙"),
   ]
 }
 
