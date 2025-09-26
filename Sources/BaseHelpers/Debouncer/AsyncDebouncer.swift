589ffef55950de47cab4fe04694059e2677efc8d
Dave Coleman <dave@dvclmn.com>
2025-09-26 14:49:01 +0200

    Have now actually *removed* `Debouncer`...
    ... and returned to AsyncDebouncer, purely because it is simpler, and I understand it slightly better. And I use it

--- Sources/BaseHelpers/Debouncer/AsyncDebouncer.swift
+++ Sources/BaseHelpers/Debouncer/AsyncDebouncer.swift
@@ -9,21 +9,12 @@ import Foundation
 
 /// Important note: Don't use the one debouncer instance for
 /// two distinct tasks, or one may cancel the other. Use seperate instances.
-@available(*, deprecated, renamed: "Debouncer", message: "This is now retired in favour of the more robust `BaseHelpers/Debouncer`")
 @MainActor
 public final class AsyncDebouncer {
   private var task: Task<Void, Never>? = nil
   private let interval: Duration
 
-  public init(interval: Duration = .seconds(0.2)) {
-    self.interval = interval
-  }
-
   public init(interval: CGFloat) {
-    self.interval = Duration.seconds(interval)
-  }
-
-  public init(interval: Int) {
     self.interval = Duration.seconds(interval)
   }
 
@@ -31,31 +22,18 @@ public final class AsyncDebouncer {
   public func execute<T: Sendable>(
     action: @escaping @Sendable () async -> T?
   ) async -> T? {
+
     /// Cancel any previous task
     task?.cancel()
 
-    /// Create a new task with the work
-    return await withCheckedContinuation { continuation in
-      task = Task { [weak self, action] in
-        guard let self = self else {
-          continuation.resume(returning: nil)
+    return await withCheckedContinuation { cont in
+      task = Task { [interval, action] in
+        try? await Task.sleep(for: interval)
+        guard !Task.isCancelled else {
+          cont.resume(returning: nil)
           return
         }
-
-        do {
-          /// Wait for the debounce interval
-          try await Task.sleep(for: self.interval)
-
-          /// If task wasn't cancelled during the wait, execute the action
-          if !Task.isCancelled {
-            let result = await action()
-            continuation.resume(returning: result)
-          } else {
-            continuation.resume(returning: nil)
-          }
-        } catch {
-          continuation.resume(returning: nil)
-        }
+        cont.resume(returning: await action())
       }
     }
   }
