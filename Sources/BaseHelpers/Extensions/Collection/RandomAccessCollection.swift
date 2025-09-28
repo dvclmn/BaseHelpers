c061282ec6cd05b89b7e3cbd69c04315e8cd2943
Dave Coleman <dave@dvclmn.com>
2025-09-28 16:03:15 +0200

    Updates

--- Sources/BaseHelpers/Extensions/Collection/RandomAccessCollection.swift
+++ Sources/BaseHelpers/Extensions/Collection/RandomAccessCollection.swift
@@ -9,27 +9,28 @@ import Foundation
 
 extension RandomAccessCollection where Element: Equatable, Index == Int {
 
-  public func element(after index: Int, wrapping: Bool = true) -> Element? {
+  public func nextElement(
+    after element: Element,
+    wrapping: Bool = true
+  ) -> Element? {
     guard !isEmpty,
-      let targetIndex = nextIndex(after: index, wrapping: wrapping)
+      let i = firstIndex(of: element),
+      let targetIndex = nextIndex(after: i, wrapping: wrapping)
     else { return nil }
+
     return self[targetIndex]
   }
 
-  public func element(before index: Int, wrapping: Bool = true) -> Element? {
-    guard !isEmpty,
-      let targetIndex = previousIndex(before: index, wrapping: wrapping)
-    else { return nil }
-    return self[targetIndex]
-  }
-  
-  public func nextElement(after element: Element, wrapping: Bool = true) -> Element? {
-    guard let i = firstIndex(of: element) else { return nil }
-    return self.element(after: i, wrapping: wrapping)
-  }
+  public func previousElement(
+    before element: Element,
+    wrapping: Bool = true
+  ) -> Element? {
 
-  public func previousElement(before element: Element, wrapping: Bool = true) -> Element? {
-    guard let i = firstIndex(of: element) else { return nil }
-    return self.element(before: i, wrapping: wrapping)
+    guard !isEmpty,
+      let i = firstIndex(of: element),
+      let targetIndex = previousIndex(before: i, wrapping: wrapping)
+    else { return nil }
+
+    return self[targetIndex]
   }
 }
