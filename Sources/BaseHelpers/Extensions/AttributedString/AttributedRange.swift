23497c11255607f361c00be8d4f041768d0334d0
Dave Coleman <dave@dvclmn.com>
2025-10-03 17:56:54 +0200

    Refinements to some range methods on AttrString

--- Sources/BaseHelpers/Extensions/AttributedString/AttributedRange.swift
+++ Sources/BaseHelpers/Extensions/AttributedString/AttributedRange.swift
@@ -8,55 +8,65 @@
 import Foundation
 
 extension AttributedString {
-  
+
   public func findMatches(for pattern: Regex<Substring>) -> [RegexMatch] {
     let string = String(self.characters)
     let matches = string.matches(of: pattern)
     return matches
   }
-  
+
   public func getRange(matching pattern: Regex<Substring>) -> AttributedRange? {
     let matches = findMatches(for: pattern)
-    for match in matches {
-      guard let range = self.range(of: match.output) else { break }
-      return range
-    }
-    return nil
+    return matches.lazy
+      .compactMap { self.range(of: $0.output) }
+      .first
   }
   
+//  public func getRange(matching pattern: Regex<Substring>) -> AttributedRange? {
+//    let matches = findMatches(for: pattern)
+//    for match in matches {
+//      guard let range = self.range(of: match.output) else { break }
+//      return range
+//    }
+//    return nil
+//  }
+
   public func getAllRanges(matching pattern: Regex<Substring>) -> [AttributedRange] {
-    let string = String(self.characters)
-    let matches = string.matches(of: pattern)
-    
-    var ranges: [Range<AttributedString.Index>] = []
-    
-    for match in matches {
-      if let range = self.range(of: match.output) {
-        ranges.append(range)
-      }
+    let matches = findMatches(for: pattern)
+    return matches.compactMap { match in
+      self.range(of: match.output)
     }
-    return ranges
+//    let string = String(self.characters)
+//    let matches = string.matches(of: pattern)
+//
+//    var ranges: [Range<AttributedString.Index>] = []
+//
+//    for match in matches {
+//      if let range = self.range(of: match.output) {
+//        ranges.append(range)
+//      }
+//    }
+//    return ranges
   }
-  
+
   public func getRange(for pattern: TripleCapture) -> TripleCaptureRange? {
-    
+
     let string = String(self.characters)
-    
+
     let matches = string.matches(of: pattern)
-    
+
     for match in matches {
       guard let range01 = self.range(of: match.output.1),
-            let range02 = self.range(of: match.output.2),
-            let range03 = self.range(of: match.output.3)
+        let range02 = self.range(of: match.output.2),
+        let range03 = self.range(of: match.output.3)
       else {
-        
+
         break
       }
-      
+
       return (range01, range02, range03)
     }
     return nil
   }
-  
-  
+
 }
